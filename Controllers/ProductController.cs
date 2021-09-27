9096f810963c9603c8214db3a6e424d114574c5a
Danilo Menezes <danilo.menezes@guide.com.br>
2021-09-27 18:47:20 -0300

    Ajustes, configuração de cors, cache

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace Shop.Controllers
 {
-    [Route("products")]
+    [Route("v1/products")]
     public class ProductController : ControllerBase
     {
         [HttpGet]
